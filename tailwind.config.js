a61ac8b08e5f452d87cfe1f3778e3555aa1e08b9
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2023-07-27 22:48:23 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "color-primary":"#01051e",
+        "color-primary-light":"#020726",
+        "color-primary-dark":"#010417",
+        "color-secondary":"#ff7d3b",
+        "color-white":"#fff",
+        "color-blob":"#AA27DF"
+      },
+
+      container:{
+        center : true,
+        padding:{
+          DEFAULT : '20px',
+          md: "150px"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
